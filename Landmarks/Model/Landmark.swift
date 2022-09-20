6958f1e8f58a628f342855817f9682ca14797898
Egor <iMrRoll@yandex.ru>
2022-09-20 18:05:13 +0300

    Add composing complex interface

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -9,10 +9,17 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Codable, Hashable, Identifiable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name, park, state, description: String
     var isFavorite: Bool
+    
+    var category: Category
+    enum Category: String, CaseIterable, Codable {
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+    }
     
     private var imageName: String
     var image: Image {
