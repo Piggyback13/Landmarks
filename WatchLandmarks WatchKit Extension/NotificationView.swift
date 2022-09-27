dc2631365c948ec6535c60b084cc3f7a66bcc2d1
Egor <iMrRoll@yandex.ru>
2022-09-27 15:21:12 +0300

    creating a watchOS App

---
+++ WatchLandmarks WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks WatchKit Extension
+//
+//  Created by Егор Глезденёв on 27.09.2022.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
